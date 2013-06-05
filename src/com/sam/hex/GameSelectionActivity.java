978623748e07a5f7be9efa7ab394a233961fa698
Will <xlythe@gmail.com>
2013-06-04 23:07:09 -0400

    clean up game selection

--- src/com/sam/hex/GameSelectionActivity.java
+++ src/com/sam/hex/GameSelectionActivity.java
@@ -1,6 +1,5 @@
 package com.sam.hex;
 
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -9,56 +8,43 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 
-public class GameSelectionActivity extends BaseGameActivity {
+import com.actionbarsherlock.app.SherlockFragmentActivity;
 
-	 public void onCreate(Bundle savedInstanceState) {
-		 super.onCreate(savedInstanceState);
-		 setContentView(R.layout.game_type);
-	        getSupportActionBar().hide();
-	        Button AI = (Button) findViewById(R.id.button1);
-	        Button hotseat = (Button) findViewById(R.id.button2);
-	        Button net = (Button) findViewById(R.id.button3);
-	        final SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
-	        final GameSelectionActivity self = this;
-	        AI.setOnClickListener(new OnClickListener(){
+public class GameSelectionActivity extends SherlockFragmentActivity {
 
-				@Override
-				public void onClick(View arg0) {
-					   startActivity(new Intent(getBaseContext(), GameActivity.class));
-					   prefs.edit().putString("player2Type", "4").apply();
-					   finish();
-					
-				}});
-	        hotseat.setOnClickListener(new OnClickListener(){
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.game_type);
+        final SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
 
-				@Override
-				public void onClick(View arg0) {
-					   startActivity(new Intent(getBaseContext(), GameActivity.class));
-					   prefs.edit().putString("player2Type", "0").apply();
-					   finish();
-				}});
-	        net.setOnClickListener(new OnClickListener(){
+        Button AI = (Button) findViewById(R.id.button1);
+        AI.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                startActivity(new Intent(getBaseContext(), GameActivity.class));
+                prefs.edit().putString("player2Type", "4").apply();
+                finish();
+            }
+        });
 
-				@Override
-				public void onClick(View arg0) {
-					   
-					   
-					   finish();
-				}});
-	        net.setEnabled(false);
-	        
-	 }
+        Button hotseat = (Button) findViewById(R.id.button2);
+        hotseat.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                startActivity(new Intent(getBaseContext(), GameActivity.class));
+                prefs.edit().putString("player2Type", "0").apply();
+                finish();
+            }
+        });
 
-	@Override
-	public void onSignInFailed() {
-		// TODO Auto-generated method stub
-		
-	}
+        Button net = (Button) findViewById(R.id.button3);
+        net.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                finish();
+            }
+        });
+        net.setEnabled(false);
 
-	@Override
-	public void onSignInSucceeded() {
-		// TODO Auto-generated method stub
-		
-	}
+    }
 }
-

58d1f40f7065b5412e1ce13e8693a0f03de5b1b0
Will <xlythe@gmail.com>
2013-06-19 23:36:59 -0400

    fix disappearing buttons on cancel

--- src/com/sam/hex/fragment/GameSelectionFragment.java
+++ src/com/sam/hex/fragment/GameSelectionFragment.java
@@ -13,14 +13,16 @@ import com.sam.hex.view.SelectorLayout;
  * @author Will Harmon
  **/
 public class GameSelectionFragment extends HexFragment {
+    private SelectorLayout mSelectorLayout;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
         View v = inflater.inflate(R.layout.fragment_game_selection, null);
 
-        SelectorLayout selectorLayout = (SelectorLayout) v.findViewById(R.id.buttons);
+        mSelectorLayout = (SelectorLayout) v.findViewById(R.id.buttons);
 
-        SelectorLayout.Button computerButton = selectorLayout.getButtons()[0];
+        SelectorLayout.Button computerButton = mSelectorLayout.getButtons()[0];
         computerButton.setColor(0xffb7cf47);
         computerButton.setText(R.string.game_selection_button_computer);
         computerButton.setOnClickListener(new SelectorLayout.Button.OnClickListener() {
@@ -33,7 +35,7 @@ public class GameSelectionFragment extends HexFragment {
             }
         });
 
-        SelectorLayout.Button hotseatButton = selectorLayout.getButtons()[1];
+        SelectorLayout.Button hotseatButton = mSelectorLayout.getButtons()[1];
         hotseatButton.setColor(0xff4ba5e2);
         hotseatButton.setText(R.string.game_selection_button_pass);
         hotseatButton.setOnClickListener(new SelectorLayout.Button.OnClickListener() {
@@ -46,7 +48,7 @@ public class GameSelectionFragment extends HexFragment {
             }
         });
 
-        SelectorLayout.Button netButton = selectorLayout.getButtons()[2];
+        SelectorLayout.Button netButton = mSelectorLayout.getButtons()[2];
         netButton.setColor(0xffcc5c57);
         netButton.setText(R.string.game_selection_button_net);
         netButton.setOnClickListener(new SelectorLayout.Button.OnClickListener() {
@@ -64,5 +66,11 @@ public class GameSelectionFragment extends HexFragment {
         });
 
         return v;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mSelectorLayout.reset();
     }
 }

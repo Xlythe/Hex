4e88b8a32bb5a747f91b7fee01bc01d1b0f2989c
Will <xlythe@gmail.com>
2016-12-28 14:45:41 -0800

    add public key for IAP

--- app/src/main/java/com/sam/hex/view/HexDialog.java
+++ app/src/main/java/com/sam/hex/view/HexDialog.java
@@ -16,7 +16,7 @@ import java.util.List;
  * @author Will Harmon
  **/
 public abstract class HexDialog extends PurchaseActivity {
-    private static final String KEY = "YOUR_KEY_HERE";
+    private static final String KEY = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAsZd0Iz6EbKEtVhrQuuQzv7023TVoZp5V/sHtKTgObbA8wrkBKpKX2W6kOu2BSCv5Lv5oMQu+fBreIXy06IoXPEFrIGZDz79UOSWra34Gc+0e3rz+73kw0ga8imWpuo5KRyO/tbeT4oLsCM44BIC8I23toMJECGiyZmwpI9qdHoci+cc/oBC6N58mtVNyqDZpAJzxCOL5AKqmSGqNRiCN0c34MXPMtP5HjC3S7G6r+bsTGii81hNThcI8qb9VrjptCJQz1gQe4TjMoSkXDQcy1d3H8AuKHosHtsuLuEI+0F+1eF7A+KkofUhOzh+Ur6dNaPE1dEKrku2zOHK7DubJ9QIDAQAB";
     public static final String ITEM_SKU_BASIC = "bronze_donation";
     public static final String ITEM_SKU_INTERMEDIATE = "silver_donation";
     public static final String ITEM_SKU_ADVANCED = "gold_donation";
